056618798c1084872d7450bc730c8c23cad6b4a0
Gergely Marincsak <gergely.marincsak@idandtrust.com>
2019-03-27 09:41:36 +0100

    GoodID .NET SDK on GitHub

---
+++ src/Helpers/ClaimChecker/AuthTimeChecker.cs
@@ -0,0 +1,74 @@
+﻿///
+/// Copyright 2017 ID&Trust, Ltd.
+///
+/// You are hereby granted a non-exclusive, worldwide, royalty-free license to
+/// use, copy, modify, and distribute this software in source code or binary form
+/// for use in connection with the web services and APIs provided by ID&Trust.
+///
+/// As with any software that integrates with the GoodID platform, your use
+/// of this software is subject to the GoodID Terms of Service
+/// (https://goodid.net/docs/tos).
+/// This copyright notice shall be included in all copies or substantial portions
+/// of the software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using GoodId.Core.Exceptions;
+
+namespace GoodId.Core.Helpers.ClaimChecker
+{
+    public class AuthTimeChecker : IClaimChecker
+    {
+        private const string ClaimName = "auth_time";
+
+        private int timeToleranceInSeconds;
+        private int requestedMaxAge;
+        private bool  authTimeRequested;
+        public AuthTimeChecker(int timeToleranceInSeconds, int requestedMaxAge = 0, bool authTmeRequested=false)
+        {
+            if (timeToleranceInSeconds < 0)
+            {
+                throw new ValidationException("Tolerance must be a non-negative number");
+            }
+            this.timeToleranceInSeconds = timeToleranceInSeconds;
+            this.requestedMaxAge = requestedMaxAge;
+            this.authTimeRequested = authTmeRequested;
+        }
+
+        public void CheckClaim(JToken token)
+        {
+            if (authTimeRequested)
+            {
+                var offset = DateTimeOffset.FromUnixTimeSeconds((long)token);
+                var authTime = offset.UtcDateTime;
+
+                if (authTime.AddSeconds((-1 * timeToleranceInSeconds)).CompareTo(DateTime.UtcNow) > 0)
+                {
+                    throw new ValidationException("The user was authenticated in the future");
+                }
+
+                var timeDiff = DateTime.UtcNow.Subtract(authTime);
+                if (timeDiff.TotalSeconds > (requestedMaxAge + timeToleranceInSeconds))
+                {
+                    throw new ValidationException("The user was authenticated too far in the past");
+                }
+            }
+                    
+        }
+
+        public string SupportedClaim()
+        {
+            return ClaimName;
+        }
+    }
+}
---
+++ src/Helpers/Response/UserinfoVerifier.cs
@@ -0,0 +1,71 @@
+﻿///
+/// Copyright 2017 ID&Trust, Ltd.
+///
+/// You are hereby granted a non-exclusive, worldwide, royalty-free license to
+/// use, copy, modify, and distribute this software in source code or binary form
+/// for use in connection with the web services and APIs provided by ID&Trust.
+///
+/// As with any software that integrates with the GoodID platform, your use
+/// of this software is subject to the GoodID Terms of Service
+/// (https://goodid.net/docs/tos).
+/// This copyright notice shall be included in all copies or substantial portions
+/// of the software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using GoodId.Core.Helpers.ClaimChecker;
+using Newtonsoft.Json;
+using Jose;
+using GoodId.Core.Exceptions;
+
+namespace GoodId.Core.Helpers.Response
+{
+    public class UserinfoVerifier
+    {
+        ClaimCheckerManager checkerManager;
+
+
+        private JObject idToken;
+        private JObject userinfo;
+
+        public UserinfoVerifier(JObject idToken, JObject userinfo)
+        {
+            this.idToken = idToken;
+            this.userinfo = userinfo;
+
+            checkerManager = new ClaimCheckerManager();
+            // OpenID specific validation
+            checkerManager.Add(new SubChecker(idToken["sub"].ToObject<string>()), true);
+
+            // GoodID specific validation
+            checkerManager.Add(new GoodIDVerifiedEmailChecker(idToken["email_hash"].ToObject<string>()));
+
+        }
+        public void Verify()
+        {
+            //Check userinfo hash
+            var normalisedUserInfo = Util.NormalizeJson(userinfo);
+            var userinfoHash = Base64Url.Encode(Util.Sha256Hash(Encoding.UTF8.GetBytes(normalisedUserInfo.ToString(Formatting.None))));
+            var expectedUserinfoHash = idToken["uih"].ToObject<string>();
+
+            if (userinfoHash.Equals(expectedUserinfoHash) == false)
+            {
+                throw new ValidationException("Unverified userinfo");
+            }
+            // Check userinfo's claims
+            checkerManager.Check(userinfo);
+        }
+    }
+
+
+}
